74df0a86eae81db1a073b01eb27704bf85aa943c
Matthias Mac <matthias-mac@refineddigital.co.nz>
2020-05-13 22:21:05 +0200

    wip

--- src/Stripe/Module/Classes/Stripe.php
+++ src/Stripe/Module/Classes/Stripe.php
@@ -3,9 +3,14 @@
 namespace RefinedDigital\PaymentGateways\Stripe\Module\Classes;
 
 use RefinedDigital\CMS\Modules\Core\Classes\PaymentGateway;
+use RefinedDigital\CMS\Modules\Core\Contracts\PaymentGatewayInterface;
 
-class Stripe extends PaymentGateway {
+class Stripe extends PaymentGateway implements PaymentGatewayInterface {
 
     protected $view = 'payment-gateways-stripe::form';
 
+    public function process($request, $cart)
+    {
+        help()->trace('processing the payment');
+    }
 }
