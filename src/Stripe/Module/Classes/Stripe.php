7f9f67b9bfa8af27b1d81156107b7f3acd8ac912
Matthias <matthias@refineddigital.co.nz>
2020-05-14 00:41:15 +0000

    adding payment processing

--- src/Stripe/Module/Classes/Stripe.php
+++ src/Stripe/Module/Classes/Stripe.php
@@ -5,12 +5,42 @@ namespace RefinedDigital\PaymentGateways\Stripe\Module\Classes;
 use RefinedDigital\CMS\Modules\Core\Classes\PaymentGateway;
 use RefinedDigital\CMS\Modules\Core\Contracts\PaymentGatewayInterface;
 
+use Omnipay\Omnipay;
+
 class Stripe extends PaymentGateway implements PaymentGatewayInterface {
 
     protected $view = 'payment-gateways-stripe::form';
 
-    public function process($request, $cart)
+    public function process($request, $form, $emailData)
     {
-        help()->trace('processing the payment');
+        // todo: update to Stripe Payment Intents at some point
+        //  this is the go away to another page to verify by the bank stuff
+
+        $gateway = Omnipay::create('Stripe');
+        $gateway->setApiKey(env('STRIPE_SECRET'));
+
+        $args = [
+            'amount' => $this->total,
+            'currency' => $this->currency,
+            'token' => $request->get('stripeToken'),
+            'description' => $this->description,
+        ];
+
+        if (sizeof($this->metaData)) {
+            $args['metadata'] = $this->metaData;
+        }
+
+        $response = $gateway
+            ->purchase($args)
+            ->send();
+
+        $transaction = $this->logTransaction($form, $emailData, $response);
+
+        $return = new \stdClass();
+        $return->success = $response->isSuccessful();
+        $return->transaction = $transaction;
+        $return->message = $response->getMessage();
+
+        return $return;
     }
 }
